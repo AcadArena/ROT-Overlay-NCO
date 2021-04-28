44e33a1741c039e04af280a680df67968451d405
Jasper Concepcion <jasper@acadarena.com>
2021-04-29 06:16:20 +0800

    feat: add codm ingame scene

--- src/view/ingame/IngameCodm.tsx
+++ src/view/ingame/IngameCodm.tsx
@@ -1,9 +1,8 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { FC } from "react";
 import { useSelector } from "react-redux";
-import LowerThirds from "../../comps/lowerthirds/LowerThirds";
 import { Participant, ReduxState } from "../../config/types/types";
-import frame from "../../assets/imgs/ingameframe.png";
+import frame from "../../assets/imgs/codm-ingame.png";
 
 const mcs = makeStyles({
   screen: {
@@ -13,106 +12,124 @@ const mcs = makeStyles({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "space-between",
+    backgroundSize: "stretch",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: `url(${frame})`,
+    position: "relative",
   },
-
   top: {
-    width: 1441,
-    height: 60,
+    width: "100%",
     display: "flex",
-    justifyContent: "space-between",
-    "& .left": { transformOrigin: "top left" },
-    "& .rightlt": {
-      transformOrigin: "top center",
-      transform: "scale(0.4761904761904762) translateX(160px)!important",
-    },
-    "& .lt": {
-      transform: "scale(0.4761904761904762)",
-      "& .team": {
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
+    justifyContent: "center",
+    transform: "translateX(14px)",
 
-        "& .logo": {
-          height: "80%",
-          width: 100,
-          margin: "0px 57px 0px 62px",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        },
+    "& .spacer": {
+      width: 519,
+      height: 69,
+    },
+
+    "& .one": {
+      "& .details": {
+        padding: "0 0 0 45px",
+      },
+      "& .score": {
+        margin: "3px 0 0 46px",
+        transform: "skew(40deg)  translateX(2px)",
+      },
+    },
+
+    "& .two": {
+      flexDirection: "row-reverse",
+      "& .details": {
+        padding: "0 45px 0 0",
+        textAlign: "right",
+        alignItems: "flex-end",
+      },
+      "& .score": {
+        margin: "3px 46px 0 0",
+        transform: "skew(-40deg) translateX(-2px)",
+      },
+    },
+
+    "& .team": {
+      display: "flex",
+      height: 69,
+      width: 288,
+
+      "& .logo": {
+        height: 60,
+        width: 60,
+        alignSelf: "center",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      },
+      "& .details-wrapper": {
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
         "& .details": {
           display: "flex",
+          justifyContent: "center",
           flexDirection: "column",
-          flex: 1,
-
+          height: 55,
           "& .school": {
-            color: "#ddd",
-            fontFamily: "industry, sans-serif",
-            textTransform: "uppercase",
-            fontSize: 26,
+            fontFamily: "Druk Wide Bold",
+            lineHeight: 1,
+            fontWeight: "bold",
+            fontSize: 20,
+          },
+          "& .org": {
+            fontFamily: "industry",
             fontWeight: "bold",
             lineHeight: 1,
-            whiteSpace: "nowrap",
-            // letterSpacing: 1,
-          },
-          "& .name": {
-            color: "#ffd200",
-            fontFamily: "Druk Wide Bold, sans-serif",
             textTransform: "uppercase",
-            fontSize: 38,
-            lineHeight: 1,
           },
-        },
-
-        "& .right": {
-          "& .school": { textAlign: "right" },
-          "& .name": { textAlign: "right" },
         },
 
         "& .score": {
-          color: "#f9f9f9",
-          fontFamily: "Druk Wide Bold, sans-serif",
-          textTransform: "uppercase",
-          fontSize: 55,
-          margin: "0px 47px",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: 5,
+          "& .bar": {
+            height: 10,
+            border: "2px solid #ffd000",
+            backgroundColor: "rgba(0,0,0,.3)",
+          },
         },
       },
     },
   },
-  bottom: {
-    height: 307,
-    width: 779,
-    backgroundSize: "100% 100%",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    backgroundImage: `url(${frame})`,
+
+  stage: {
+    position: "absolute",
+    height: 50,
+    width: 300,
+    bottom: 195,
+    left: 49,
     display: "flex",
-    flexDirection: "column",
     alignItems: "center",
-    "& .text": {
-      marginTop: 18,
-      width: 700,
-      // backgroundColor: "rgba(0,0,0,.5)",
-      textAlign: "center",
-      fontFamily: "Druk Wide Bold",
-      textTransform: "uppercase",
-      fontSize: 21,
-      color: "#f8f8f8",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: 66.2,
-    },
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,.5)",
+
+    fontFamily: "Anton",
+    color: "#fff",
+    textTransform: "uppercase",
+    fontSize: 35,
   },
 });
 
 const IngameCodm = () => {
   const c = mcs();
-  const { tournament, match, swap_team_positions, live_data } = useSelector(
-    (state: ReduxState) => state.live
-  );
+  const {
+    tournament,
+    match,
+    swap_team_positions: swap,
+    live_data,
+  } = useSelector((state: ReduxState) => state.live);
 
-  const getParticipant = (id: number): Participant | undefined => {
+  const team = (id: number = 0): Participant | undefined => {
     return (
       tournament?.participants.find((p) => p.id === id) ??
       tournament?.participants.find((p) => p.group_player_ids.includes(id))
@@ -128,7 +145,7 @@ const IngameCodm = () => {
     );
   };
 
-  const getFinalScore = (score: string, teamIndex: number) => {
+  const getFinalScore = (score: string = "0-0", teamIndex: 1 | 2) => {
     const scores: string[] = score.split(",");
     let team1: number = 0;
     let team2: number = 0;
@@ -146,9 +163,94 @@ const IngameCodm = () => {
 
   return (
     <div className={c.screen}>
-      <div className={c.top}></div>
+      <div className={c.top}>
+        <div className="team one">
+          <div
+            className="logo"
+            style={{
+              backgroundImage: `url(${getTeamLogo(
+                swap ? match?.player2_id : match?.player1_id
+              )})`,
+            }}
+          ></div>
+          <div className="details-wrapper">
+            <div className="details">
+              <div className="school">
+                {
+                  team(swap ? match?.player2_id : match?.player1_id)
+                    ?.university_acronym
+                }
+              </div>
+              <div className="org">
+                {team(swap ? match?.player2_id : match?.player1_id)?.org_name}
+              </div>
+            </div>
+            <div className="score">
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 2 : 1) > 0}
+              />
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 2 : 1) > 1}
+              />
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 2 : 1) > 2}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="spacer"></div>
+        <div className="team two">
+          <div
+            className="logo"
+            style={{
+              backgroundImage: `url(${getTeamLogo(
+                swap ? match?.player1_id : match?.player2_id
+              )})`,
+            }}
+          ></div>
+          <div className="details-wrapper">
+            <div className="details">
+              <div className="school">
+                {
+                  team(swap ? match?.player1_id : match?.player2_id)
+                    ?.university_acronym
+                }
+              </div>
+              <div className="org">
+                {team(swap ? match?.player1_id : match?.player2_id)?.org_name}
+              </div>
+            </div>
+            <div className="score">
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 1 : 2) > 2}
+              />
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 1 : 2) > 1}
+              />
+              <Bar
+                shaded={getFinalScore(match?.scores_csv, swap ? 1 : 2) > 0}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/**
+       * This section is for the lower left info
+       */}
+
+      <div className={c.stage}>{live_data?.stage}</div>
     </div>
   );
 };
 
 export default IngameCodm;
+
+const Bar: FC<{ shaded?: boolean }> = ({ shaded }) => {
+  return (
+    <div
+      className="bar"
+      style={{ backgroundColor: shaded ? "#ffd000" : "rgba(0,0,0,.3)" }}
+    />
+  );
+};
