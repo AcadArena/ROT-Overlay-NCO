a781f28d2884bed289d3cd232aa7009312035435
Jasper Concepcion <onetruemanok@gmail.com>
2021-09-10 20:02:34 +0800

    test fix

--- src/config/WebsocketProvider.tsx
+++ src/config/WebsocketProvider.tsx
@@ -18,7 +18,7 @@ let config = {
   host: "",
   path: "",
 };
-const hostCloud: string = "https://servers.acadarena.com/ws";
+const hostCloud: string = "https://servers.acadarena.com/";
 const hostLocal: string = "localhost:3200";
 
 if (window.location.hostname === "localhost") {
